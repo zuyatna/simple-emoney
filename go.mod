048c9976b3ee2497b7d27dad124e5476460773d1
zuyatna <zuyatna@gmail.com>
2025-06-30 13:55:03 +0700

    adding auth service

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module simple-emoney
 go 1.24.3
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.11.0
+	golang.org/x/crypto v0.39.0
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/redis/go-redis/v9 v9.11.0 // indirect
 )
---
+++ internal/app/service/auth_service.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"simple-emoney/config"
+	"simple-emoney/internal/app/repository"
+	"simple-emoney/internal/model"
+	"simple-emoney/pkg/utils"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService interface {
+	RegisterUser(req *model.RegisterRequest) (*model.User, error)
+	LoginUser(req *model.LoginRequest) (*model.LoginResponse, error)
+}
+
+type authService struct {
+	userRepo  repository.UserRepository
+	redisRepo repository.RedisRepository
+	cfg       *config.Config
+}
+
+func NewAuthService(userRepo repository.UserRepository, redisRepo repository.RedisRepository, cfg *config.Config) AuthService {
+	return &authService{
+		userRepo:  userRepo,
+		redisRepo: redisRepo,
+		cfg:       cfg,
+	}
+}
+
+func (s *authService) RegisterUser(req *model.RegisterRequest) (*model.User, error) {
+	existingUser, err := s.userRepo.GetUserByUsername(req.Username)
+	if err != nil {
+		return nil, err
+	}
+	if existingUser != nil {
+		return nil, errors.New("username already exists")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	user := &model.User{
+		Username:     req.Username,
+		PasswordHash: string(hashedPassword),
+		Balance:      0,
+	}
+
+	err = s.userRepo.CreateUser(user)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	s.redisRepo.SetUserCache(user, 1*time.Hour)
+
+	return user, nil
+}
+
+func (s *authService) LoginUser(req *model.LoginRequest) (*model.LoginResponse, error) {
+	user, err := s.userRepo.GetUserByUsername(req.Username)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %w", err)
+	}
+	if user == nil {
+		return nil, errors.New("user not found")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(req.Password))
+	if err != nil {
+		return nil, errors.New("invalid password")
+	}
+
+	token, err := utils.GenerateJWTToken(user.ID, user.Username, s.cfg.JWTSecretKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate token: %w", err)
+	}
+
+	s.redisRepo.SetAuthToken(token, user.ID.String(), 24*time.Hour)
+
+	return &model.LoginResponse{
+		UserID:   user.ID.String(),
+		Username: user.Username,
+		Token:    token,
+	}, nil
+}
