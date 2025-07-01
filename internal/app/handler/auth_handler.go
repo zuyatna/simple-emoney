8a1d9d25945df2221050d744084881d45bf223b8
zuyatna <zuyatna@gmail.com>
2025-07-01 13:50:32 +0700

    adding auth handler

--- internal/app/handler/auth_handler.go
+++ internal/app/handler/auth_handler.go
@@ -1,1 +1,52 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"simple-emoney/internal/app/service"
+	"simple-emoney/internal/model"
+)
+
+type AuthHandler struct {
+	authService service.AuthService
+}
+
+func NewAuthHandler(authService service.AuthService) *AuthHandler {
+	return &AuthHandler{authService: authService}
+}
+
+func (a *AuthHandler) RegisterUser(c *gin.Context) {
+	var req model.RegisterRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := a.authService.RegisterUser(&req)
+	if err != nil {
+		log.Printf("Failed to register user: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "User registered successfully",
+		"user_id": user.ID,
+	})
+}
+
+func (a *AuthHandler) LoginUser(c *gin.Context) {
+	var req model.LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := a.authService.LoginUser(&req)
+	if err != nil {
+		log.Printf("Failed to login user: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, response)
+}
